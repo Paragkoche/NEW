5b2b4e3fe01a3b2137951aa79d87f4cb1d2333d6
paragkoche <paragkoche0@gmail.com>
2025-04-15 20:04:12 +0530

    refactor: convert Page component to use hooks for data fetching and state management

--- app/src/app/(app)/page.tsx
+++ app/src/app/(app)/page.tsx
@@ -1,25 +1,37 @@
-// app/gallery/page.tsx (or similar)
+"use client";
+
+import { useEffect, useState } from "react";
 import { getModels } from "@/api";
 import Card from "./_components/Card";
 import { Product } from "@/types/config";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API;
 
-const Page = async () => {
-  const data = await getModels();
+const Page = () => {
+  const [columns, setColumns] = useState<
+    [Product[], Product[], Product[], Product[]]
+  >([[], [], [], []]);
 
-  // Split into 4 columns
-  const columns: [Product[], Product[], Product[], Product[]] = [
-    [],
-    [],
-    [],
-    [],
-  ];
-  data.data.models.forEach((model, idx) => {
-    console.log(columns);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getModels();
 
-    columns[idx % 4].push(model);
-  });
+      // Split into 4 columns
+      const newColumns: [Product[], Product[], Product[], Product[]] = [
+        [],
+        [],
+        [],
+        [],
+      ];
+      data.data.models.forEach((model, idx) => {
+        newColumns[idx % 4].push(model);
+      });
+
+      setColumns(newColumns);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
