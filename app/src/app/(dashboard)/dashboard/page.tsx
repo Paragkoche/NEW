3db88e4e0dedc130a88cdf7abc414fc4460aaa9c
paragkoche <paragkoche0@gmail.com>
2025-04-15 20:01:14 +0530

    fix: update dashboard page to correctly fetch and display models data

--- app/src/app/(dashboard)/dashboard/page.tsx
+++ app/src/app/(dashboard)/dashboard/page.tsx
@@ -1,25 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { getModels } from "@/api";
 import Card from "./_components/cards";
-// import Header from "./_components/header";
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API;
-const page = async () => {
-  const data = await getModels();
-  // console.log(data.data);
+
+const Page = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getModels();
+      setData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <>
-      <main className="grid grid-cols-1 sm:grid-cols-3 gap-2 p-3">
-        {data.data.models.map((v, i) => (
-          <Card
-            name={v.name}
-            imageUrl={`${API_BASE_URL}/${v.thumbnailUrl}`}
-            key={i}
-            id={v.id}
-          />
-        ))}
-      </main>
-    </>
+    <main className="grid grid-cols-1 sm:grid-cols-3 gap-2 p-3">
+      {data.models.map((v: any, i: number) => (
+        <Card
+          name={v.name}
+          imageUrl={`${API_BASE_URL}/${v.thumbnailUrl}`}
+          key={i}
+          id={v.id}
+        />
+      ))}
+    </main>
   );
 };
 
-export default page;
+export default Page;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,12 @@ import app from "./app";
 import env from "./utility/env";
 import logger from "./utility/logger";
 import db from "./db";
-const Port = env.PORT!;
-app.listen(Port, async () => {
+const Port = parseInt(env.PORT!);
+app.listen(Port, "0.0.0.0", async () => {
   console.clear();
   logger.info("connecting to db...");
 
   await db.$connect();
   logger.info("db connected..");
-  logger.info("server start");
+  logger.info("server start " + Port);
 });
